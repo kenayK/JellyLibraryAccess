50b8fc1dec4f0e733da338f3359c0d2c9e9bf83d
kenayK <111674051+kenayK@users.noreply.github.com>
2026-08-15 18:15:03 +1000

    Use neutral JellyLibraryAccess approval tag

--- Jellyfin.Plugin.JellyLibraryAccess/Configuration/PluginConfiguration.cs
+++ Jellyfin.Plugin.JellyLibraryAccess/Configuration/PluginConfiguration.cs
@@ -5,7 +5,7 @@ namespace Jellyfin.Plugin.JellyLibraryAccess.Configuration;
 
 public class PluginConfiguration : BasePluginConfiguration
 {
-    public string ApprovalTag { get; set; } = "kenay-kids-approved";
+    public string ApprovalTag { get; set; } = "jellylibraryaccess-approved";
     public List<Guid> BaselineLibraryIds { get; set; } = [];
     public List<MovieApproval> ApprovedMovies { get; set; } = [];
     public bool RemoveOrphanedApprovalTags { get; set; } = true;
